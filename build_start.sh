769a8ee043671bbb14c5d3e80d82a3435eb01e0f
andreytrotsik <a.trotsik@krasnodar.pro>
2025-03-04 16:01:07 +0300

    add build_start.sh

---
+++ build_start.sh
@@ -0,0 +1,5 @@
+docker build . -t webapp
+docker tag webapp cr.yandex/crppkeb0hbjp6i480150/webapp:latest
+docker push cr.yandex/crppkeb0hbjp6i480150/webapp:latest
+docker pull cr.yandex/crppkeb0hbjp6i480150/webapp:latest
+docker run -it -p 80:5005 --name webapp cr.yandex/crppkeb0hbjp6i480150/webapp:latest
